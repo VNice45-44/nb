1e5ae3a1bc7286fe13cec1d8d0e7eb9fa502c75a
VNice45-44 <kelvinhiuhu@hotmail.com>
2026-07-29 02:44:31 +0300

    fix: render login route inside layout

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,18 +4,9 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue'), meta: { requiresAuth: true } },
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
+      { path: ':catchAll(.*)*', component: () => import('pages/ErrorNotFound.vue') },
     ],
-  },
-  {
-    path: '/login',
-    component: () => import('pages/LoginPage.vue'),
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
   },
 ]
 
